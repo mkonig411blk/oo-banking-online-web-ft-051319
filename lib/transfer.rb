7787ede9e87476aa124d2f845e0c32ae8447790b
Margot Konig <margot.konig@blackrock.com>
2019-06-03 03:16:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,7 +12,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.valid?
+    if @sender.valid? && @status = "pending"
     @sender.balance -= @amount
     @receiver.balance += @amount
     @status = "complete"
@@ -24,8 +24,8 @@ class Transfer
   
   def reverse_transfer
     if @status = "complete"
-    @sender.balance += amount
-    @receiver.balance -= amount
+    @sender.balance += @amount
+    @receiver.balance -= @amount
   else 
   end
   end
